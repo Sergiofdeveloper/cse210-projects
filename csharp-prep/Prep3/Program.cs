97291c091dec5e44c6d51887bd83d00e3a26629f
Sergiofdeveloper <flo22013@byui.edu>
2023-04-24 19:09:36 +0200

    prep 3 solved

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        
+       Random randomGenerator = new Random(); 
+       int number = randomGenerator.Next (1,11); 
+        int guess= 1; 
+
+        while (number != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (number > guess)
+            {
+                Console.WriteLine("Higer");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("lower");
+            }
+            else            
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }  
 }
