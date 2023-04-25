056eaf81e3dd8413eb0b95e7490518590f0746f2
Sergiofdeveloper <flo22013@byui.edu>
2023-04-25 22:59:34 +0200

    Prep 5 exercises

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void greeting()
+        {
+          Console.WriteLine("Welcome to the Program!");
+        }
+    static string UserName ()
+       {
+         Console.Write("Please enter your name: ");
+         string InputName = Console.ReadLine();
+         return InputName;
+      }   
+
+    static int UserNumber ()
+       {
+         Console.Write("Please enter your favorite number: ");
+         int InputNumber = int.Parse (Console.ReadLine());
+         return InputNumber;
+      }
+
+      static int SquareNumber(int InputNumber)
+      {
+        int Square = InputNumber * InputNumber;
+        return Square;
+
+      }
+      static void TotalNumber(string InputName, int Square)
+      {
+        Console.WriteLine($"{InputName}, the square of your number is {Square}");
+      } 
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+          greeting();
+          string Name = UserName();
+          int Number = UserNumber();
+          int Square = SquareNumber(Number); 
+          TotalNumber(Name, Square);
+        
     }
 }
