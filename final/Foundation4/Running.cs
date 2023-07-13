801530f8b6870b48b78fc2c2cfa7f730ce54a751
Sergiofdeveloper <flo22013@byui.edu>
2023-07-13 09:52:41 +0200

    upload running class

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -1,1 +1,31 @@
-{\rtf1}
+public class Running : Activity
+{
+    public double Distance { get; set; }
+
+    public override double Distance()
+    {
+        return Distance;
+    }
+
+    public override double Speed()
+    {
+        return (Distance / LengthInMinutes) * 60;
+    }
+
+    public override double Pace()
+    {
+        return LengthInMinutes / Distance;
+    }
+
+    public override string Summary()
+    {
+        double distance = Distance();
+        double speed = Speed();
+        double pace = Pace();
+
+        string summary = $"{Date:dd MMM yyyy} Running ({LengthInMinutes} min) - " +
+                         $"Distance: {distance} miles, Speed: {speed} mph, Pace: {pace} min per mile";
+
+        return summary;
+    }
+}
